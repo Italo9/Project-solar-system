35065122a40483a90535ad7452fa20cd3faf9782
ítalo Lima <contatoitallo@hotmail.com>
2022-02-19 21:43:39 -0300

    Renderiza uma lista com as missões espaciais-Req 10

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -8,11 +8,13 @@ class MissionCard extends React.Component {
     const { country } = this.props;
     const { destination } = this.props;
     return (
-      <div data-testid="mission-card">
-        <p data-testid="mission-name">{name}</p>
-        <p data-testid="mission-year">{year}</p>
-        <p data-testid="mission-country">{country}</p>
-        <p data-testid="mission-destination">{destination}</p>
+      <div>
+        <div data-testid="mission-card">
+          <p data-testid="mission-name" key={ name }>{name}</p>
+          <p data-testid="mission-year" key={ name }>{year}</p>
+          <p data-testid="mission-country" key={ name }>{country}</p>
+          <p data-testid="mission-destination" key={ name }>{destination}</p>
+        </div>
       </div>
     );
   }
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
-        <Title headline="Missões" />
+      <div>
+        <div data-testid="missions">
+          <Title headline="Missões" />
+        </div>
+        <section>
+          {missions.map((missionPosicao) => (
+            <MissionCard
+              key={ missionPosicao.name }
+              name={ missionPosicao.name }
+              year={ missionPosicao.year }
+              country={ missionPosicao.country }
+              destination={ missionPosicao.destination }
+            />
+          ))}
+        </section>
       </div>
     );
   }
